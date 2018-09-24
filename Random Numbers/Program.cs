af4b6d131ebb864a92766d53e0562e7ce8382998
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-09-24 11:37:49 -0400

    add random numbers

--- Random Numbers/Program.cs
+++ Random Numbers/Program.cs
@@ -10,6 +10,24 @@ namespace Random_Numbers
     {
         static void Main(string[] args)
         {
+            var random = new Random();
+            int num1 = random.Next(1, 10);
+            int num2 = random.Next(1, 10);
+            //  Console.WriteLine("what is your first number?");
+            //  int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //  Console.WriteLine("what is your  number?");
+            //  int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Add(num1, num2));
+            Console.WriteLine(Subtract(num1, num2));
+            Console.WriteLine(Multiply(num1, num2));
+            Console.WriteLine();
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
         }
+        private static int Multiply(int num1, int num2) => num1 * num2;
+        private static int Subtract(int num1, int num2) => num1 - num2;
+        private static int Add(int num1, int num2) => num1 + num2;
     }
 }
